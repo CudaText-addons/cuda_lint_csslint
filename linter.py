dd7c844b6fe25e7ad0e21ae51f0d304b372f103d
Alexey-T <support@uvviewsoft.com>
2016-02-29 21:03:32 +0300

    init

---
+++ linter.py
@@ -0,0 +1,59 @@
+#
+# linter.py
+# Linter for CudaLint
+#
+# Original: by Aparajita Fishman
+# Copyright (c) 2013 Aparajita Fishman
+# Change: Alexey T. (CudaText)
+#
+# License: MIT
+#
+
+"""This module exports the CSSLint plugin linter class."""
+
+import os
+from SublimeLinter.lint import Linter, util
+
+_node = 'node' if os.name=='nt' else 'nodejs'
+_js = os.path.join(os.path.dirname(__file__), 'node_modules', 'csslint', 'cli.js')
+
+class CSSLint(Linter):
+    """Provides an interface to the csslint."""
+
+    syntax = 'CSS'
+    cmd = (_node, _js, '--format=compact')
+    regex = r'''(?xi)
+        ^.+:\s*   # filename
+
+        # csslint emits errors that pertain to the code as a whole,
+        # in which case there is no line/col information, so that
+        # part is optional.
+        (?:line\ (?P<line>\d+),\ col\ (?P<col>\d+),\ )?
+        (?:(?P<error>error)|(?P<warning>warning))\ -\ (?P<message>.*)
+    '''
+    word_re = r'^([#\.]?[-\w]+)'
+    error_stream = util.STREAM_BOTH
+    tempfile_suffix = 'css'
+    defaults = {
+        '--errors=,': '',
+        '--warnings=,': '',
+        '--ignore=,': ''
+    }
+    inline_overrides = ('errors', 'warnings', 'ignore')
+    comment_re = r'\s*/\*'
+
+    def split_match(self, match):
+        """
+        Extract and return values from match.
+
+        We override this method so that general errors that do not have
+        a line number can be placed at the beginning of the code.
+        """
+
+        match, line, col, error, warning, message, near = super().split_match(match)
+
+        if line is None and message:
+            line = 0
+            col = 0
+
+        return match, line, col, error, warning, message, near
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Plugin for CudaLint.
+It adds support for CSS lexer.
+
+**** Needs Node.js in PATH - for Win32 it's "node.exe", for Linux it's "nodejs" 
+It uses CSSLint tool, included in this package, it's installed via "npm install csslint".
+
+Author: Alexey (Synwrite)
+License MIT
